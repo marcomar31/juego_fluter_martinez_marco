35a28c94fef8394f7f652587bae3e62d99382041
Marco Martínez <marcomar3104gaming@gmail.com>
2024-01-12 21:34:48 +0100

    Commit 8: Añadidas capas con gotas y estrellas.

--- lib/games/MyGame.dart
+++ lib/games/MyGame.dart
@@ -2,8 +2,8 @@ import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
-
-import '../configs/config.dart';
+import '../elementos/Estrella.dart';
+import '../elementos/Gota.dart';
 import '../players/EmberPlayer.dart';
 
 class MyGame extends FlameGame {
@@ -36,6 +36,21 @@ class MyGame extends FlameGame {
     mapComponent = await TiledComponent.load('mapa2.tmx', Vector2.all(16));
     world.add(mapComponent);
 
+    ObjectGroup? estrellas=mapComponent.tileMap.getLayer<ObjectGroup>("estrellas");
+
+    for(final estrella in estrellas!.objects) {
+      Estrella spriteStar = Estrella(position: Vector2(estrella.x*1.5,estrella.y*1.45),
+          size: Vector2(64, 64));
+      add(spriteStar);
+    }
+
+    ObjectGroup? gotas=mapComponent.tileMap.getLayer<ObjectGroup>("gotas");
+
+    for(final gota in gotas!.objects){
+      Gota spriteGota = Gota(position: Vector2(gota.x*1.5,gota.y*1.45),
+          size: Vector2(64,64));
+      add(spriteGota);
+    }
     _ember = EmberPlayer(
       position: Vector2(60, 360),
     );
